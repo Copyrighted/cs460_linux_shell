e813941c8430fc41f41b153e4bde361e8ef3d2c4
TS <tylerscheffler@protonmail.com>
2019-12-05 14:11:45 -0800

    Got login to work..

--- login.c
+++ login.c
@@ -4,20 +4,18 @@ int in, out, err;
 char uname[128], password[128];
 char *tokens[7];
 
-void tokenize(char *line)
+int tokenize(char *line, char *buf[], char delim)
 {
-    char *cp;
-    cp = line;
+    char *cp = line;
+    int len = 0;
 
-    argc = 0;
-
-    while (*cp != 0)  //while cp is not null
+    while (*cp != 0) //while cp is not null
     {
-        while (*cp == ':')
+        while (*cp == ' ')
             *cp++ = 0; //make null
-        if (*cp != 0) 
-            tokens[argc++] = *cp;
-        while (*cp != ':' && *cp != 0)
+        if (*cp != 0)
+            buf[len++] = cp;
+        while (*cp != delim && *cp != 0)
             cp++;
         if (*cp != 0)
             *cp = 0;
@@ -25,49 +23,59 @@ void tokenize(char *line)
             break;
         cp++;
     }
-    tokens[argc] = 0;
+    return len;
 }
 
 main(int argc, char *argv[])
 {
-    close(0); //close stdin
-    close(1); //close stdout
+    char *lines[128], *acc_info[128], file_lines[1024];
+
+    close(0); //close stdin why?!
+    close(1); //close stdout  why?!?!
+
     in = open(argv[1], 0);
-    out = open(argv[1], 1), err = open(argv[1], 2);
+    out = open(argv[1], 1);
+    err = open(argv[1], 2);
+
     settty(argv[1]); //set tty name string in PROC.tty
-    int fd = open("/etc/passwd", O_RDONLY);
+
     while (1)
     {
+        int passwd_fd = open("/etc/passwd", O_RDONLY);
+        print2f(" ");
+        print2f("LOGIN YA PIECE OF SHIT");
+        print2f(" ");
         prints("login:");
         gets(uname);
         print2f("password:");
         gets(password);
-        uname[strlen(uname) - 1] = '\0';
-        password[strlen(password) - 1] = '\0';
-        char line[256];
+        //uname[strlen(uname) - 1] = '\0';
+        //password[strlen(password) - 1] = '\0';
+        read(passwd_fd, file_lines, 1024);
 
-        while (getline_fd(line, fd))
+        int len = tokenize(file_lines, lines, '\n');
+
+        for (int i = 0; i < len; i++)   
         {
+            tokenize(lines[i], acc_info, ':');
+            
+            if (strcmp(uname, acc_info[0]) == 0 && strcmp(password, acc_info[1]) == 0)
+            {
+                // change uid, gid to user's uid, gid;
+                chuid(atoi(acc_info[2]), atoi(acc_info[3]));
 
-            //for each line in etc/pass file do
-            //tokenize user account line
-            //print2f("tokenizing");
-            tokenize(line);
-            //printf("username %s\n", tokens[0]);
-            //printf("password %s\n", tokens[1]);
-            if ((strcmp(tokens[0], uname) == 0) && (strcmp(tokens[1], password) == 0))
-            { //username:password:gid:uid:fullname:HOMEDIR:program
-                prints("FUCK YOU");
-                //change uid, gid to user's uid, gid  //chuid
-                chuid(atoi(tokens[3]), atoi(tokens[2]));
-                //change cwd to users home DIR
-                chdir(tokens[6]);
-                //close opened /etc/password file
-                close(fd);
-                //exec to program in user account
-                exec(tokens[7]);
+                // change cwd to users home DIR
+                chdir(acc_info[5]);
+
+                // close opened /etc/passwd file
+                close(passwd_fd);
+
+                // exec to program in user account // exec
+                exec(acc_info[6]);
+
+                break;
             }
         }
+        printf("Login failed, try again\n");
     }
-    printf("Login failed, try again\n");
 }
